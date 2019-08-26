1deb797c0f7e95c93d1ee331b8422bce2fc89d5c
Kel Kanhirun <kel.kanhirun@gmail.com>
2019-08-26 14:05:55 -0400

    Separates Array<TimePeriod> from TimePeriod

---
+++ External/Engine/Sources/Engine/[TimePeriod]+JSON.swift
@@ -0,0 +1,25 @@
+import SwiftDate
+import SwiftyJSON
+
+public extension Array where Element : TimePeriod {
+    func toJSON() -> JSON {
+        let arr = self.map { $0.toJSON() }
+        
+        return JSON(arr)
+    }
+    
+    static func fromJSON(_ json: JSON) -> [TimePeriod] {
+        guard let arr = json.array else {
+            return [TimePeriod]()
+        }
+        
+        let mapped = arr.map { jsonPeriod -> TimePeriod in
+            let start = DateInRegion(jsonPeriod["start"].stringValue)!
+            let end = DateInRegion(jsonPeriod["end"].stringValue)!
+            
+            return TimePeriod(start: start, end: end)
+        }
+        
+        return mapped
+    }
+}
