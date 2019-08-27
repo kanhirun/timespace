2f3156697f6dee2094bb602777a4aebd9e2e1f9d
Kel Kanhirun <kel.kanhirun@gmail.com>
2019-08-26 21:30:09 -0400

    Pushes down JSON func to the right layer

--- External/Engine/Sources/Engine/TimePeriod+JSON.swift
+++ External/Engine/Sources/Engine/TimePeriod+JSON.swift
@@ -2,6 +2,14 @@ import SwiftDate
 import SwiftyJSON
 
 extension TimePeriod {
+    
+    convenience init(fromJSON json: JSON) {
+        let start = DateInRegion(json["start"].stringValue)!
+        let end = DateInRegion(json["end"].stringValue)!
+
+        self.init(start: start, end: end)
+    }
+    
     func toJSON() -> JSON {
         return JSON([
             "start": start!.toISO(),
--- External/Engine/Sources/Engine/[TimePeriod]+JSON.swift
+++ External/Engine/Sources/Engine/[TimePeriod]+JSON.swift
@@ -3,23 +3,14 @@ import SwiftyJSON
 
 public extension Array where Element : TimePeriod {
     func toJSON() -> JSON {
-        let arr = self.map { $0.toJSON() }
-        
+        let arr = map { $0.toJSON() }
+
         return JSON(arr)
     }
     
     static func fromJSON(_ json: JSON) -> [TimePeriod] {
-        guard let arr = json.array else {
-            return [TimePeriod]()
+        return (json.arrayValue).map {
+            TimePeriod(fromJSON: $0)
         }
-        
-        let mapped = arr.map { jsonPeriod -> TimePeriod in
-            let start = DateInRegion(jsonPeriod["start"].stringValue)!
-            let end = DateInRegion(jsonPeriod["end"].stringValue)!
-            
-            return TimePeriod(start: start, end: end)
-        }
-        
-        return mapped
     }
 }
