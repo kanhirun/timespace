65b8d489378cecab1983960c0128f9cb4f91b8d1
Kel Kanhirun <kel.kanhirun@gmail.com>
2019-08-27 23:51:53 -0400

    Adds dyanmic data to viewmodel

--- imessage/TimeSheet/TimeSheet+ViewModels.swift
+++ imessage/TimeSheet/TimeSheet+ViewModels.swift
@@ -1,63 +1,56 @@
 import SwiftDate
 import Foundation
 
+let periods = [
+    TimePeriod(start: DateInRegion("2019-08-01T10:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-01T09:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-01T08:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-01T07:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-01T06:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-01T05:00:00Z")!, duration: 1.hours),
+    
+    TimePeriod(start: DateInRegion("2019-08-02T10:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-02T11:00:00Z")!, duration: 1.hours),
+    
+    TimePeriod(start: DateInRegion("2019-08-03T20:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-03T21:00:00Z")!, duration: 1.hours),
+    TimePeriod(start: DateInRegion("2019-08-03T22:00:00Z")!, duration: 1.hours),
+]
+
 struct ViewModel {
 
     let subHeaderViewModels: [HeaderViewModel]
-    let cellViewModels: [ [CellViewModel] ]
+    let cellViewModels: [[CellViewModel]]
     
     init() {
-        self.subHeaderViewModels = [
-            HeaderViewModel(DateComponents(month: 1, day: 12, weekday: 1)),
-            HeaderViewModel(DateComponents(month: 1, day: 13, weekday: 2)),
-            HeaderViewModel(DateComponents(month: 1, day: 14, weekday: 3)),
-        ]
-
-        self.cellViewModels = [
-            [
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-12T01:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-12T02:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-12T03:00:00Z")!, end: DateInRegion())),
-            ],
-            [
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-13T11:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-13T12:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-13T13:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-13T14:00:00Z")!, end: DateInRegion())),
-            ],
-            [
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-14T20:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-14T21:00:00Z")!, end: DateInRegion())),
-                CellViewModel(TimePeriod(start: DateInRegion("2019-01-14T22:00:00Z")!, end: DateInRegion())),
-            ],
-        ]
+        (self.subHeaderViewModels, self.cellViewModels) = ViewModel.generateModels(periods: periods)
     }
     
-    func generate() {
-//        guard periods.count > 0 else {
-//            return
-//        }
-//
-//        // Pointer for tracking whether a new column should be created
-//        var currDay: Int? = nil
-//        var currColumn: UIStackView!
-//
-//        periods.forEach { period in
-//            if (currDay == nil || period.start!.day > currDay! ) {
-//                currColumn = createColumnView(period)
-//
-//                addArrangedSubview(currColumn!)
-//
-//                currDay = period.start?.day
-//            }
-//
-//            let timeCell: AppointmentCell = .fromNib()
-//            timeCell.updateUI(period)
-//            timeCell.timeButton.addTarget(self, action: #selector(TimeSheetView.selectAppointment(sender:)), for: .primaryActionTriggered)
-//
-//            currColumn.addArrangedSubview(timeCell)
-//        }
+    private static func generateModels(periods: [TimePeriod]) -> ( [HeaderViewModel], [[CellViewModel]] ) {
+        var headers = [HeaderViewModel]()
+        var cells = [ [CellViewModel] ]()
+
+        var curr: Int? = nil
+        var headerIndex = -1
+
+        periods.forEach { period in
+            if (curr == nil || period.start!.day > curr! ) {
+                let newHeader = HeaderViewModel(period.start!.dateComponents)
+                
+                headers.append(newHeader)
+                cells.append([])
+                
+                curr = period.start?.day
+                headerIndex += 1
+            }
+            
+            let cell = CellViewModel(period)
+            cells[headerIndex].append(cell)
+        }
+        
+        return (headers, cells)
     }
+
 }
 
 struct HeaderViewModel {
